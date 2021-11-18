b0173df02a911feb5c79ec3dd06c6165b71d95d4
chenzhihao <chenzhihao@bailian.ai>
2021-11-18 17:52:40 +0800

    reformat the file

--- experiments/single_test/globalpointer_test_sujianlin.py
+++ experiments/single_test/globalpointer_test_sujianlin.py
@@ -26,20 +26,8 @@ from bert4keras.snippets import sequence_padding, DataGenerator
 from bert4keras.snippets import open, to_array
 from keras.models import Model
 from tqdm import tqdm
-import tensorflow as tf
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-# gpus = tf.config.experimental.list_physical_devices('GPU')
-# if gpus:
-#   # Restrict TensorFlow to only use the first GPU
-#   try:
-#     tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-#     tf.config.experimental.set_memory_growth(gpus[0], True)
-#     logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-#     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-#   except RuntimeError as e:
-#     # Visible devices must be set before GPUs have been initialized
-#     print(e)
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 maxlen = 256
 epochs = 100
